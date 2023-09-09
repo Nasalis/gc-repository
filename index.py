db8b4126f020907e75b052775d103b847c24fb0c
Davi dos Santos Freitas <davidossantosfreitas19@gmail.com>
2023-09-09 15:05:43 -0300

    feat: add multiplication function

--- index.py
+++ index.py
@@ -1,5 +1,10 @@
 def sum(a, b):
     return a + b
 
+def multiplication(a, b):
+    return a * b
+
 result = sum(2, 5)
-print(f'Sum a + b: {result}') 
+result2 = multiplication(2, 5)
+print(f'Sum a + b: {result}')
+print(f'Multi a + b: {result2}')
